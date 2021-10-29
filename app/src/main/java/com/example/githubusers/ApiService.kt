dc22ea3b6024425df79ca52961a78ca581c762a0
Zulham Dahlan <56948314+zulham-dahlan@users.noreply.github.com>
2021-10-30 00:19:57 +0700

    Remove Api Token
    Menghapus token API Github saya, silahkan your-token diganti dengan token anda.

--- app/src/main/java/com/example/githubusers/ApiService.kt
+++ app/src/main/java/com/example/githubusers/ApiService.kt
@@ -8,25 +8,25 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token your-token")
     fun searchUser(
         @Query("q") login: String
     ): Call<GithubResponse>
 
     @GET("users/{login}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token your-token")
     fun getDetailUser(
         @Path("login") login: String
     ): Call<UserGithub>
 
     @GET("users/{login}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token your-token")
     fun getFollowers(
         @Path("login") login: String
     ): Call<ArrayList<UserGithub>>
 
     @GET("users/{login}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token your-token")
     fun getFollowing(
         @Path("login") login: String
     ): Call<ArrayList<UserGithub>>
